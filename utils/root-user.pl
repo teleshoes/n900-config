c1e7f36e90fdb7dd7b6ceb7c95fb5f416a4d0de6
Elliot Wolk <elliot.wolk@gmail.com>
2011-08-25 20:27:14 -0400

    added a script which configures the root user in a few ways, sharing some things between user and root

---
+++ utils/root-user.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+my $date = `date +%Y-%m-%d_%s`;
+chomp $date;
+
+my @files = ('.bashrc', '.profile', '.vimrc',
+             'bin', 'resolvconf', 'openvpn',
+             '.cpulimits', '.twip');
+for my $file(@files){
+  my $bakfile = $file . "_$date.bak";
+
+  my $cmd;
+
+  $cmd = "ssh root@`n900` '" .
+   "gfind /root/$file -type l -delete; " .
+   "mv /root/$file /root/$bakfile; " .
+   "ln -s /home/user/$file /root/$file'";
+  print "$cmd\n";
+  system $cmd;
+}
