3967f0fdc5443a0151cb60aaad97e20ffcbe5030
Elliot Wolk <elliot.wolk@gmail.com>
2011-09-01 21:42:21 -0400

    added a few additional backup utils, for cavestory, rtcom event logger, lmarbles, and cleaned up the code a bit

--- utils/backup_restore.pl
+++ utils/backup_restore.pl
@@ -15,26 +15,54 @@ sub prompt($){
   }
 }
 
-if(prompt 'Are you absolutely sure you know what youre doing?'){
-  my $latest = '/media/mmc1/Backup/Latest';
-  my $link = `ssh root@\`n900\` ls -l $latest`;
-  if($link =~ /-> \/media\/mmc1\/Backup\/([0-9_-]+)\n$/){
-    print "!!! Latest backup date is: $1\n";
-  }else{
-    print "!!! Unknown backup date; is microSD mounted?\n";
-  }
+my $backupDir = '/media/mmc1/Backup';
 
-  print "This will DESTROY all your current contacts.\n";
-  print "To continue, type 'destroy contacts' without the quotes\n";
-  my $response = <STDIN>;
-  chomp $response;
-  if(lc $response eq 'destroy contacts'){
-    my $file = "$latest/osso-addressbook-backup";
-    system "ssh root@`n900` 'echo " .
-      "\"osso-addressbook-backup -i $file\"" .
-      " | su - user'";
-  }else{
-    print "skipped\n";
-  }
+print "fetching the latest dir, will prompt before doing anything\n";
+my $lDir = "$backupDir/Latest";
+my $link = `ssh root@\`n900\` ls -l $lDir`;
+if($link =~ /-> $backupDir\/([0-9_-]+)\n$/){
+  $lDir = "$backupDir/$1";
+  print "Latest backup date is: $1\n";
+}else{
+  die "Unknown backup date; is microSD mounted?\n";
 }
 
+if(prompt "OVERWRITE contacts with osso-addressbook-backup?"){
+  my $file = "$lDir/osso-addressbook-backup";
+  system "ssh root@`n900` 'echo " .
+    "\"osso-addressbook-backup -i $file\"" .
+    " | su - user'";
+}
+
+if(prompt "OVERWRITE cavestory save profile(s)?"){
+  my $dest = '/home/user/MyDocs/Games/CaveStory/n900';
+  system "ssh root@`n900` cp -a $lDir/home/user/CaveStory/* $dest";
+  system "ssh root@`n900` chown user.users $dest -R";
+}
+
+if(prompt "OVERWRITE lmarbles profile and save?"){
+  my @files = (
+    '/home/user/.lgames/lmarbles.conf',
+    '/home/user/.lmarbles_profile',
+    '/home/user/.lmarbles_state');
+  my $cmd = '';
+  for my $file(@files){
+    $cmd .= "cp $lDir/$file $file; ";
+  }
+  $cmd .= "chown user.users /home/user/.lmarbles* /home/user/.lgames -R; ";
+  system "ssh root@`n900` '$cmd'";
+}
+
+if(prompt "Copy rtcom event logger db {texts/calls/etc}? backup will be made"){
+  my $dir = "/home/user/.rtcom-eventlogger";
+  my $srcFile = "$lDir/$dir/el-v1.db";
+  my $destFile = "$dir/el-v1.db";
+  my $backupFile = "$dir/el-v1.db.bak" . time;
+  print "Backup will be made at $backupFile\n";
+  system "ssh root@`n900` '" .
+    "cp $destFile $backupFile; " .
+    "cp $srcFile $destFile'";
+  if(prompt "restart hildon desktop {may fail, restart}?"){
+    system "ssh root@`n900` killall hildon-desktop";
+  }
+}
