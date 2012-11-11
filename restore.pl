e019608c392eecc8428308e65a848bcd86732fca
Elliot Wolk <elliot.wolk@gmail.com>
2012-11-11 11:36:45 -0500

    moved gcc install to a separate script

--- restore.pl
+++ restore.pl
@@ -201,19 +201,6 @@ sub sync_claws_mail(){
 #####
 
 sub install_others(){
-  if(ask 'install gcc-4.2 & g++-4.2 from SDK repo {disabled after}, and add links?'){
-    system "ssh root@`n900` '" .
-      "cd /etc/apt/sources.list.d; " .
-      "mv sdk.list.disabled sdk.list; " .
-      "apt-get update; " .
-      "apt-get install gcc-4.2 g++-4.2; " .
-      "mv sdk.list sdk.list.disabled; " .
-      "apt-get update; " .
-      "ln -s /usr/bin/gcc-4.2 /usr/bin/gcc; " .
-      "ln -s /usr/bin/gcc-4.2 /usr/bin/cc; " .
-      "ln -s /usr/bin/g++-4.2 /usr/bin/g++; " .
-      "'";
-  }
 }
 
 
